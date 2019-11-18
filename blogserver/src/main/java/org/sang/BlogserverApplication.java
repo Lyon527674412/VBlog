2bb01457a09b7e31f000e5b6dbf4bc6755798b86
Lyon <527674412@qq.com>
2019-11-18 11:22:55 +0800

    添加mariaDB的依赖，修改了数据库URL

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.sql.*;
+
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
 public class BlogserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogserverApplication.class, args);
+
+
     }
 }
